be686f6362e4083e61a02682d393be1a34bf6c0c
KABANGA David <Davkabanga96@gmail.com>
2025-01-20 22:29:59 +0200

    Setting up the user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 import { Schema, model, models } from 'mongoose';
 
-const userSchema = newSchema({
+const userSchema = new Schema({
     email: {
         type: String,
         unique: [true, 'Email already exists!'],
@@ -11,5 +11,13 @@ const userSchema = newSchema({
         type: String,
         required: [true, 'Username is required!'],
         match: [/^(?=.{8,20}$()?![_.]()?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, 'Username invalid, it should contain 8 - 20 alphanumeric letters and be unique!'],
+    },
+
+    image: {
+        type: String,
     }
 })
+
+const User = model("User", userSchema);
+
+export default User;
