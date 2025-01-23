60dd29ca4daecd42ffc9be319e40a654c28861ec
KABANGA David <Davkabanga96@gmail.com>
2025-01-23 18:57:00 +0200

    Fixing bugs of while creating a new prompt

--- app/create_prompt/page.jsx
+++ app/create_prompt/page.jsx
@@ -8,6 +8,9 @@ import Form from '@components/Form';
 
 const Createprompt = () => {
 
+    const router = useRouter();
+    const { data: session } = useSession();
+
     const [submitting, setSubmitting] = useState(false);
     const [post, setPost] = useState({
         prompt: '',
@@ -29,7 +32,7 @@ const Createprompt = () => {
             })
 
             if(response.ok) {
-                Router.push('/')
+                router.push('/')
             }
         } catch (error) {
             console.log(error);
