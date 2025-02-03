b2effa639654c67f7659b58365ddd6a4ed898e25
KABANGA David <Davkabanga96@gmail.com>
2025-02-04 01:10:10 +0200

    Implementing the profile page

---
+++ app/profile/page.jsx
@@ -0,0 +1,30 @@
+'use client'
+
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import Profile from '@components/profile'
+
+const Profile = () => {
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+        
+    }
+
+  return (
+    <Profile
+        name="My"
+        desc="Welcome to your personalized profile page"
+        data={[]}
+        handleEdit={}
+        handleDelete={}
+    />
+  )
+}
+
+export default Profile
